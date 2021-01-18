14c7e977419d3b6f9dd209fa39ea690c759264b0
pclank <panosclank@gmail.com>
2021-01-18 20:42:23 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/BackPropagation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BackPropagation"
+  "BackPropagation.pdb"
+  "CMakeFiles/BackPropagation.dir/ebp.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BackPropagation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ebp.c
@@ -0,0 +1,32 @@
+// ********************************************************************************
+// First Serial Implementation of Error Back - Propagation Neural Network Algorithm
+// ********************************************************************************
+
+// Include Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Definitions - Macros
+
+
+// Declare Arrays
+double WL1[100][13];    // First Neural Level Weights
+double WL2[10][101];    // Second Neural Level Weights
+double DL1[100];        // First Neural Level State
+double DL2[10];         // Second Neural Level State
+double OL1[100];        // First Neural Level Output
+double OL2[10];         // Second Neural Level Output
+
+// Function to Activate Neural Network
+void activateNN(void)                   // TODO: Possibly Add Parameter Option
+{
+
+}
+
+// Driver Function
+int main(void)
+{
+
+    return 0;
+}
