2745ec8103f6043957100dd2ec9f1cb92e626ac2
pclank <panosclank@gmail.com>
2021-01-18 21:30:52 +0200

    Added Various Helper Functions.

--- ebp.c
+++ ebp.c
@@ -5,18 +5,36 @@
 // Include Libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
 // Definitions - Macros
 
 
 // Declare Arrays
-double WL1[100][13];    // First Neural Level Weights
-double WL2[10][101];    // Second Neural Level Weights
-double DL1[100];        // First Neural Level State
-double DL2[10];         // Second Neural Level State
-double OL1[100];        // First Neural Level Output
-double OL2[10];         // Second Neural Level Output
+double WL1[100][13];    // First Neural Layer Weights
+double WL2[10][101];    // Second Neural Layer Weights
+double DL1[100];        // First Neural Hidden Layer
+double DL2[10];         // Second Neural Hidden Layer
+double OL1[100];        // First Neural Layer Output
+double OL2[10];         // Second Neural Layer Output
+
+// ***********************************
+// Helper Functions
+// ***********************************
+
+double sigmoid(double x)
+{
+    return 1 / (1 + exp(-x));
+}
+double dSigmoid(double x)
+{
+    return x * (1 - x);
+}
+double init_weight(void)
+{
+    return ((double)rand())/((double)RAND_MAX);
+}
 
 // Function to Activate Neural Network
 void activateNN(void)                   // TODO: Possibly Add Parameter Option
